0b70e722695f81086f38adbc4b2c71520fe21dff
Diego Dario <diegodario88@gmail.com>
2021-08-25 12:04:17 -0300

    feat: Layout e electron-forge

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 612,
+    height: 850,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "index.html"));
 }
 
 app.whenReady().then(() => {
--- render.js
+++ render.js
@@ -2,31 +2,6 @@ const btnTrigga = document.getElementById("btnTrigga");
 const caixaResultado = document.getElementById("caixaResultado");
 const formulario = document.getElementById("formulario");
 
-const payload = {
-  name: "Philip Moss Mccray",
-  gender: "Soluta doloremque do",
-  age: 36,
-  hobby: "Blanditiis excepturi",
-  email: "12ssf3bicygecefo@mailinator.com",
-  bio: "Repellendus Molesti",
-  birthday: "1984-12-06",
-  avatar: "",
-  proficiency: [
-    {
-      skill: "JavaScript",
-      rate: 50,
-    },
-    {
-      skill: "NodeJs",
-      rate: 60,
-    },
-    {
-      skill: "CSS",
-      rate: 60,
-    },
-  ],
-};
-
 btnTrigga.addEventListener("click", (evt) => {
   evt.preventDefault();
   const elementos = formulario.elements;
